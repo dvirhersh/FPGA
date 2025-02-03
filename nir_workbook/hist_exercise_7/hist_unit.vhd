cde9a39f75bb63e02f623b97fb122a1e29bac19a
Dvir Hershkovits <dvirhersh@gmail.com>
2025-02-03 14:19:27 +0200

    Add states
    Signed-off-by: Dvir Hershkovits <dvirhersh@gmail.com>

--- nir_workbook/hist_exercise_7/hist_unit.vhd
+++ nir_workbook/hist_exercise_7/hist_unit.vhd
@@ -42,32 +42,31 @@ end HIST_UNIT;
 
 architecture Behavioral of HIST_UNIT is
 
-    constant COLLECTION_TIME   : integer := 2046;
-    constant PRESENTATION_TIME : integer := 2302; -- 256
-    constant ZEROING_TIME      : integer := 2558;
+    type state_type is (COLLECT, PRESENT, ZERO);
+    signal state : state_type := COLLECT;
+
+    constant COLLECTION_TIME   : integer := 2046; -- 1023 * 2
+    constant PRESENTATION_TIME : integer := 2302; -- +256
+    constant ZEROING_TIME      : integer := 2558; -- +256
 
     COMPONENT blk_mem_gen_0 -- ROM
-      PORT (
-        clka  : IN STD_LOGIC;
-        addra : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
-        douta : OUT STD_LOGIC_VECTOR(7 DOWNTO 0) 
-      );
+      PORT (clka  : IN STD_LOGIC;
+            addra : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
+            douta : OUT STD_LOGIC_VECTOR(7 DOWNTO 0));
     END COMPONENT;
     
     COMPONENT blk_mem_gen_1 -- Singal Dual Port RAM
-      PORT (
-        clka  : IN STD_LOGIC;
-        wea   : IN STD_LOGIC_VECTOR(0 DOWNTO 0);
-        addra : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
-        dina  : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
-        clkb  : IN STD_LOGIC;
-        addrb : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
-        doutb : OUT STD_LOGIC_VECTOR(9 DOWNTO 0) 
-      );
+      PORT (clka  : IN STD_LOGIC;
+            wea   : IN STD_LOGIC_VECTOR(0 DOWNTO 0);
+            addra : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+            dina  : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
+            clkb  : IN STD_LOGIC;
+            addrb : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+            doutb : OUT STD_LOGIC_VECTOR(9 DOWNTO 0));
     END COMPONENT;
     
-    signal counter : std_logic_vector (11 downto 0) := (others => '1');
-    signal counter_delayed : std_logic_vector (11 downto 0) := (others => '1');
+    signal counter           : std_logic_vector (11 downto 0) := (others => '1');
+    signal counter_delayed   : std_logic_vector (11 downto 0) := (others => '1');
     signal counter_delayed_b : std_logic_vector (11 downto 0) := (others => '1');
     
     -- For ROM
@@ -77,14 +76,13 @@ architecture Behavioral of HIST_UNIT is
     signal wea   : STD_LOGIC_VECTOR(0 DOWNTO 0) := (others => '0');
     signal addra : STD_LOGIC_VECTOR(7 DOWNTO 0) := (others => '0');
     signal dina  : STD_LOGIC_VECTOR(9 DOWNTO 0) := (others => '0');
-    signal addrb : STD_LOGIC_VECTOR(7 DOWNTO 0) := (others => '0');
-    -- out
-    signal doutb_sig : STD_LOGIC_VECTOR(9 DOWNTO 0) := (others => '0');
+    signal doutb : STD_LOGIC_VECTOR(9 DOWNTO 0) := (others => '0');
+
+    signal addrb               : STD_LOGIC_VECTOR(7 DOWNTO 0) := (others => '0');
+    signal addrb_delayed       : STD_LOGIC_VECTOR(7 DOWNTO 0) := (others => '0');
+    signal addrb_delayed_again : STD_LOGIC_VECTOR(7 DOWNTO 0) := (others => '0');
 
     signal hist_ready_int : std_logic := '0';
-    
-    signal addrb_delayed : STD_LOGIC_VECTOR(7 DOWNTO 0) := (others => '0');
-    signal addrb_delayed_again : STD_LOGIC_VECTOR(7 DOWNTO 0) := (others => '0');
 
 begin
 
@@ -112,22 +110,22 @@ begin
     end process; 
     
     -- Main control process
-    process (counter_delayed, douta, doutb_sig) begin
-        -- Collection phase
+    process (counter_delayed, douta, doutb) begin
         if counter_delayed < COLLECTION_TIME then
+            state <= COLLECT;
             addra  <= douta;
             addrb  <= douta;
             wea(0) <= counter_delayed(0);
-            dina   <= doutb_sig + 1;
+            dina   <= doutb + 1;
             hist_ready_int <= '0';
         elsif counter_delayed <= PRESENTATION_TIME then
-            -- Presentation phase
             wea(0) <= '0';
             -- addrb  <= counter - COLLECTION_TIME; -- counter 0 - 255
             addrb  <= std_logic_vector(to_unsigned((to_integer(unsigned(counter_delayed)) - COLLECTION_TIME), 8));
             hist_ready_int <= '1';
+            state <= PRESENT;
         else
-            -- Zeroing phase
+            state <= ZERO;
             wea(0) <= '1';
             dina   <= (others => '0');
             -- addra  <= counter - PRESENTATION_TIME; -- counter 0 - 255
@@ -139,24 +137,19 @@ begin
    -- Output assignments
     HIST_READY   <= hist_ready_int;     
     HIST_VALUE   <= addrb_delayed_again;
-    VALUE_AMOUNT <= doutb_sig;
+    VALUE_AMOUNT <= doutb;
     
     ROM : blk_mem_gen_0
-      PORT MAP (
-        clka  => CLK,
-        addra => counter(10 downto 1),
-        douta => douta
-      );
-    
+      PORT MAP (clka  => CLK,
+                addra => counter(10 downto 1),
+                douta => douta);
     SDPR : blk_mem_gen_1
-      PORT MAP (
-        clka  => CLK,
-        wea   => wea,
-        addra => addra,
-        dina  => dina,
-        clkb  => CLK,
-        addrb => addrb,
-        doutb => doutb_sig
-      );
+      PORT MAP (clka  => CLK,
+                wea   => wea,
+                addra => addra,
+                dina  => dina,
+                clkb  => CLK,
+                addrb => addrb,
+                doutb => doutb);
 
 end Behavioral;
